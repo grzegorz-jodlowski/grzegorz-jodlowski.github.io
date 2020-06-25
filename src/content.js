809603c36fcfdaf6863c6f4893b75694dc8a8165
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-25 19:01:06 +0200

    Add content.js file with page content

---
+++ src/content.js
@@ -0,0 +1,13 @@
+export const content = {
+  heroTitle: 'Hey there! 👋 My name is Grzegorz and I’m an aspiring Frontend developer, Pharmacist and mountain lover.',
+  heroDescription: 'You might be interested in checking the list of my skills. Don’t forget to have a glimpse at my projects. Currently I’m looking for a new challenges so if you have one - contact me 😊',
+  email: 'grzegorz.jodlowski@gmail.com',
+  workingKnowledge: [],
+  knowSomething: [],
+  wantToLearn: [],
+  projectDescription: 'I started my frontend developer path in 2019. Since then, I’ve built some projects. Here’s the list of the things I’ve done so far.',
+  contactDescription: 'I’m open for new opportunities. If you have one for me we should talk!',
+  facebook: 'https://www.facebook.com/jodlowski.grzegorz',
+  github: 'https://github.com/grzegorz-jodlowski',
+  linkedIn: 'https://www.linkedin.com/in/grzegorz-jod%C5%82owski/',
+};
