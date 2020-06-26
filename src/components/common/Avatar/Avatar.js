46292c57647de54493fadc5ff8ba0537e58b9eb6
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-26 18:18:57 +0200

    Add avatar to Hero component

---
+++ src/components/common/Avatar/Avatar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import styles from './Avatar.module.scss';
+
+const Component = ({ className }) => (
+  <div className={clsx(className, styles.root)}>
+    <img src='avatar.jpg' alt='Avatar właściciela strony' className={styles.image} />
+  </div>
+);
+
+Component.propTypes = {
+  className: PropTypes.string,
+};
+
+export {
+  Component as Avatar,
+  Component as AvatarComponent, //for tests
+};
+
--- src/components/features/Hero/Hero.js
+++ src/components/features/Hero/Hero.js
@@ -4,11 +4,13 @@ import clsx from 'clsx';
 
 import styles from './Hero.module.scss';
 
+import { Avatar } from '../../common/Avatar/Avatar';
+
+
 const Component = ({ className, children }) => (
   <section className={clsx(className, styles.root)}>
     <div className={'container'}>
-      <h2>Hero</h2>
-      {children}
+      <Avatar />
     </div>
   </section>
 );
