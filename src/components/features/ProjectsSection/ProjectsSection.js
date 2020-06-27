53343a23fc055a2fc238ce442a96aaac4ace2eaf
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-27 17:48:41 +0200

    Refactor ProjectSection comp to class comp

--- src/components/features/ProjectsSection/ProjectsSection.js
+++ src/components/features/ProjectsSection/ProjectsSection.js
@@ -5,21 +5,43 @@ import clsx from 'clsx';
 import styles from './ProjectsSection.module.scss';
 
 import { Title } from '../../common/Title/Title';
+import { Projects } from '../Projects/Projects';
 
 import { content } from '../../../content';
 
 
-const Component = ({ className }) => (
-  <section id='projects' className={clsx(className, styles.root)}>
-    <div className={'container'}>
-      <Title text='Recent projects' size='big' />
-      <p className={styles.description}>{content.projectDescription}</p>
-    </div>
-  </section>
-);
+class Component extends React.Component {
+  state = {
+    loading: false,
+    projects: [],
+  }
+
+  componentDidMount() {
+    if (this.state.projects.length) {
+      // fetch()
+      //   .then
+    }
+  }
+
+  render() {
+    const { className, projects } = this.props;
+
+    return (
+      <section id='projects' className={clsx(className, styles.root)} >
+        <div className={'container'}>
+          <Title text='Recent projects' size='big' />
+          <p className={styles.description}>{content.projectDescription}</p>
+          <Projects projects={projects} />
+        </div>
+      </section>
+    );
+  }
+}
+
 
 Component.propTypes = {
   className: PropTypes.string,
+  projects: PropTypes.array,
 };
 
 export {
