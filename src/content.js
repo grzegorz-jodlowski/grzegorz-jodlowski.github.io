4017f8508c35982d1ef322f9e51a533059262a9b
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-27 15:21:59 +0200

    Update content about skills

--- src/content.js
+++ src/content.js
@@ -1,9 +1,11 @@
 export const content = {
   heroTitle: 'Hey there! 👋 My name is Grzegorz and I’m Frontend developer, Pharmacist and mountain lover.',
   email: 'grzegorz.jodlowski@gmail.com',
-  workingKnowledge: [],
-  knowSomething: [],
-  wantToLearn: [],
+  skills: {
+    workingKnowledge: ['Semantic HTML5', 'Flexbox', 'Markdown', 'JS: Local storage', 'Figma', 'CSS grid', 'Forms', 'JS: DOM manipulations', 'SASS'],
+    knowSomething: ['JavaScript ES6+ features', 'CSS animations', 'Working with API', 'NPM Scripts', 'Progressive web apps', 'Git', 'Accessibility'],
+    wantToLearn: ['React JS', 'Node.JS', 'Electron', 'Server side rendering', 'Typescript', 'Gatsby', 'Websockets'],
+  },
   projectDescription: 'I started my frontend developer path in 2019. Since then, I’ve built some projects. Here’s the list of the things I’ve done so far.',
   contactDescription: 'I’m open for new opportunities. If you have one for me we should talk!',
   facebook: 'https://www.facebook.com/jodlowski.grzegorz',
