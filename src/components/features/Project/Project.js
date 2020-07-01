41833e5801b55fe754d9776e6a35cb6e3714d0f6
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-07-01 16:41:27 +0200

    Add stars for favourites project

--- src/components/features/Project/Project.js
+++ src/components/features/Project/Project.js
@@ -9,27 +9,32 @@ import { nameFromDashedTitle } from '../../../utils/nameFromDashedTitle';
 import { Title } from '../../common/Title/Title';
 import { Link } from '../../common/Link/Link';
 
-const Component = ({ className, name, description, homepage }) => (
-  <div className={clsx(className, styles.root)}>
-    <div className={styles.header}>
-      <i className={clsx(styles.githubLogo, 'fab fa-github')}></i>
-      <Title className={styles.title} text={nameFromDashedTitle(name)} size='small' />
-      <p className={styles.subtitle}>{description}</p>
+const Component = ({ className, name, description, homepage, html_url, id }) => {
+  const starredProjects = [269592157, 266321545, 252203276, 216858353, 240736461, 254131413, 260241952, 236449516];
+  return (
+    <div className={clsx(className, styles.root)}>
+      {starredProjects.includes(id) && <i className={clsx(styles.star, 'fas fa-star')}></i>}
+      <div className={styles.header}>
+        <i className={clsx(styles.githubLogo, 'fab fa-github')}></i>
+        <Title className={styles.title} text={nameFromDashedTitle(name)} size='small' />
+        <p className={styles.subtitle}>{description}</p>
 
+      </div>
+      <div className={styles.links}>
+        {homepage && <Link icon='desktop' text='Demo' source={homepage} />}
+        {html_url && <Link icon='code' text='Github' source={html_url} separator={homepage ? true : false} />}
+      </div>
     </div>
-    <div className={styles.links}>
-      <Link icon='desktop' text='Demo' source={homepage} />
-      <Link icon='code' text='Github' source={homepage} />
-    </div>
-
-  </div>
-);
+  );
+};
 
 Component.propTypes = {
   className: PropTypes.string,
   name: PropTypes.string,
   description: PropTypes.string,
   homepage: PropTypes.string,
+  html_url: PropTypes.string,
+  id: PropTypes.number,
 };
 
 export {
