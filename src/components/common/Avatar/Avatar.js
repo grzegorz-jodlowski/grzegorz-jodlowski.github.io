9b7ab2431a46c981167467892a6f8ffab1f66eb0
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-07-04 13:51:06 +0200

    Implement Bubble to Avatar

--- src/components/common/Avatar/Avatar.js
+++ src/components/common/Avatar/Avatar.js
@@ -4,8 +4,11 @@ import clsx from 'clsx';
 
 import styles from './Avatar.module.scss';
 
+import { Bubble } from '../Bubble/Bubble';
+
 const Component = ({ className }) => (
   <div className={clsx(className, styles.root)}>
+    <Bubble text='Hi there!' icon='👋' className={styles.bubble} />
     <img src='avatar.jpg' alt="Owner's avatar" className={styles.image} />
   </div>
 );
