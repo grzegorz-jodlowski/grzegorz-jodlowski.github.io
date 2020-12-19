d3b9a85ceffa496c4dbf5d3c04cdea445c557a19
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-12-19 14:08:06 +0100

    Update name display for SKJ project

--- src/utils/nameFromDashedTitle.js
+++ src/utils/nameFromDashedTitle.js
@@ -1,6 +1,8 @@
 export const nameFromDashedTitle = (dashedTitle) => {
   if (dashedTitle === 'grzegorz-jodlowski.github.io') {
     return 'This website - my portfolio';
+  } else if (dashedTitle === 'system-kontroli-jakosci-w-budownictwie') {
+    return 'Quality control system in construction industry';
   } else {
 
     return dashedTitle[0].toUpperCase() + dashedTitle.slice(1).split('-').join(' ');
