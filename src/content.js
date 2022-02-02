f0ac948e484c679b305aa86c367a6b9f402f7915
Grzegorz <grzegorz.jodlowski@gmail.com>
2022-02-02 17:41:05 +0100

    Content update

--- src/content.js
+++ src/content.js
@@ -2,8 +2,8 @@ export const content = {
   heroTitle: 'My name is Grzegorz and I’m Frontend developer, healthy lifestyle enthusiast and mountain lover.',
   email: 'grzegorz.jodlowski@gmail.com',
   skills: {
-    workingKnowledge: ['React', 'Redux', 'Express.js', 'MongoDB', 'Typescript', 'JavaScript ES6+ features', 'SASS & CSS Modules', 'Material-UI', 'Progressive web apps', 'Jira', 'Git', 'GitHub'],
-    knowSomething: ['Next.js', 'Electron', 'Jest/Enzyme', 'Mocha/Chai', 'Websockets', 'Figma'],
+    workingKnowledge: ['React', 'Next.js', 'Redux', 'Express.js', 'MongoDB', 'Typescript', 'JavaScript ES6+ features', 'SASS & CSS Modules', 'Material-UI', 'Progressive web apps', 'Jira', 'Git', 'GitHub/GitLab/Bitbucket'],
+    knowSomething: ['Cypress',  'Jest', 'Electron', 'Websockets', 'Figma'],
     wantToLearn: ['Nest.js', 'GraphQL', 'React native'],
   },
   projectDescription: 'I started my frontend developer path in 2019. Since then, I’ve built some projects. Here’s the list of the things I’ve done so far.',
